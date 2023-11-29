ef691aca3619a820fcbe364abe7b6e3a7610da57
farzana-yesmin <farzana_cse15@yahoo.com>
2023-11-30 00:24:54 +0600

    first commit

---
+++ src/main/java/com/rakib/graphqlspringintegration/GraphqlSpringIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.rakib.graphqlspringintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlSpringIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlSpringIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rakib/graphqlspringintegration/GraphqlSpringIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rakib.graphqlspringintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlSpringIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
